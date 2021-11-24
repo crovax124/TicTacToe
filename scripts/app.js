88ac9760b587e637b66efc57d5c2b6ff64e7e25d
crovax124 <eric.isensee11@gmail.com>
2021-11-24 21:13:22 +0100

    Winner Check, restart Logic, draw check, game over status

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
+const gameData = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+];
+
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
 
 const players = [
   {
@@ -16,11 +25,16 @@ const backdropElement = document.getElementById("backdrop");
 const formElement = document.getElementById("name-input");
 const errorsOutput = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById('active-game');
+const playerNameSpanElement = document.getElementById('active-player-name');
+const gameOverElement = document.getElementById('game-over');
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li");  //list item selector
+const gameBoardELement = document.getElementById('game-board'); // list selector
+
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -30,3 +44,10 @@ formElement.addEventListener("submit", savePlayerConfig);
 
 
 startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//     gameFieldElement.addEventListener('click', selectGameField);
+// }
+
+gameBoardELement.addEventListener('click', selectGameField);
+
